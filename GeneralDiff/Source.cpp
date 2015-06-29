553e4f76b23df6770dbfd32bc14fcbcd6f323918
jg-maon <maomona.0513@gmail.com>
2015-06-30 02:01:58 +0900

    first commit

---
+++ GeneralDiff/Source.cpp
@@ -0,0 +1,13 @@
+
+namespace GeneralDiff
+{
+	int Execute(int argc, const char* argv[])
+	{
+		return 0;
+	}
+}
+
+int main(int argc, const char* argv[])
+{
+	return GeneralDiff::Execute(argc, argv);
+}
